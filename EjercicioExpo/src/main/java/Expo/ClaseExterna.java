a927f81bc45c75ef134569ade496f0bc52fdaf15
ArnulfoJimenez <87824514+ArnulfoJimenez@users.noreply.github.com>
2022-04-28 07:23:57 -0600

    Ejercicio

---
+++ EjercicioExpo/src/main/java/Expo/ClaseExterna.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Expo;
+
+/**
+ *
+ * @author Josss_k4ptalp
+ */
+public class ClaseExterna {
+    
+      public static void Suma(int n, int m) {
+
+        System.out.println("La suma de n y m es : " + (n + m));
+    }
+
+    public static void Resta(int a, int b) {
+
+        System.out.println("La resta de a y b es : " + (a - b));
+    }
+
+    public static void Multiplicacion(int h, int i) {
+
+        System.out.println("La multiplicaion de h y i es : " + (h * i));
+
+    }
+    
+    
+}
---
+++ EjercicioExpo/src/main/java/Expo/ClaseProcedimiento.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Expo;
+
+import static Expo.ClaseExterna.Multiplicacion;
+import static Expo.ClaseExterna.Resta;
+import static Expo.ClaseExterna.Suma;
+
+/**
+ *
+ * @author Josss_k4ptalp
+ */
+public class ClaseProcedimiento {
+    
+    public static void main(String[]args)
+    {
+ 
+        System.out.println("Los resultados son: ");
+        
+        Suma(2, 2);
+        
+        Resta(10,5);
+        
+        Multiplicacion(10,10);
+        
+        
+    }
+}
